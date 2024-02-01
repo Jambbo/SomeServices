4422cab0db334b536665869cc05185a9941cea48
Hlib Chibichian <chibgleb99@gmail.com>
2024-02-01 15:39:45 +0000

    cm

--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -8,6 +8,7 @@ import com.example.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,17 +19,29 @@ import java.util.stream.Collectors;
 @Transactional
 public class OrderService {
 
+    private final WebClient webClient; // uses for async web requests
     private final OrderRepository orderRepository;
 
     public void placeOrder(OrderRequest orderRequest){
-
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
-        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList().stream()
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
                 .map(this::mapOrderLineItemsFromDto).toList();
         order.setOrderLineItemsList(orderLineItems);
-        orderRepository.save(order);
+        // Call inventory-service, and place order if product is in stock
+        Boolean result = webClient.get() //get method because in InventoryController there is a GetMapping
+                .uri("http://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();//webclient.get(...    - it makes an async request to the link that is in .uri() to inventory endpoint
+        if(result){
+            orderRepository.save(order);
+        }else{
+            throw new IllegalArgumentException("Product is not in stock please try again later");
+        }
+
     }
 
     private OrderLineItems mapOrderLineItemsFromDto(OrderLineItemsDto orderLineItemsDto) {
